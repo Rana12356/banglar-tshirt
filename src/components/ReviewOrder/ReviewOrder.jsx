a7507fe81e3a387e701548b113cc07479f81d459
Rana12356 <rana15-12356@diu.edu.bd>
2024-01-08 14:22:29 +0600

    initial data loaded

---
+++ src/components/ReviewOrder/ReviewOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewOrder = () => {
+    return (
+        <div>
+            <h2>Review Order Page</h2>
+        </div>
+    );
+};
+
+export default ReviewOrder;
